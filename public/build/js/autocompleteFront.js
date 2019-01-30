ef31677d7b7d83655bec7b85fd138e361cf5792d
david <davidgastaldello@msn.com>
2019-01-30 09:42:25 +0100

    delete autocomplete front

--- public/build/js/autocompleteFront.js
+++ public/build/js/autocompleteFront.js
@@ -1,19 +1,19 @@
-const completeIds = [
-    'sr'
-];
-
-completeIds.forEach(function (e) {
-    if (document.getElementById(e)) {
-        new autoComplete({
-            selector: 'input[id="' + e + '"]',
-            minChars: 3,
-            source: function (term, response) {
-                $.getJSON('/autocomplete/all?' + e + '=' + term, {},
-                    function (data) {
-                        response(data);
-                    }
-                );
-            }
-        });
-    }
-});
+// const completeIds = [
+//     'sr'
+// ];
+//
+// completeIds.forEach(function (e) {
+//     if (document.getElementById(e)) {
+//         new autoComplete({
+//             selector: 'input[id="' + e + '"]',
+//             minChars: 3,
+//             source: function (term, response) {
+//                 $.getJSON('/autocomplete/all?' + e + '=' + term, {},
+//                     function (data) {
+//                         response(data);
+//                     }
+//                 );
+//             }
+//         });
+//     }
+// });
