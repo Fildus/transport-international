df1c5d6d90caf01552d21dad8727ea8eadd9fd0d
david <davidgastaldello@msn.com>
2019-02-26 16:49:01 +0100

    Landing page system

--- src/EventSubscriber/RedirectSubscriber.php
+++ src/EventSubscriber/RedirectSubscriber.php
@@ -6,16 +6,29 @@ use App\Controller\Front\ProfessionalProfileController;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 
 class RedirectSubscriber extends AbstractController implements EventSubscriberInterface
 {
+    /**
+     * @param FilterControllerEvent $event
+     *
+     * @return void
+     */
     public function onKernelController(FilterControllerEvent $event)
     {
         $kernelHost = $event->getRequest()->getHttpHost();
         if ((int)preg_match('#^www#', $kernelHost) === 0) {
             header('Location: ' . 'https://www.' . $kernelHost, true, 301);
             exit();
+        }
+
+        $file = $event->getRequest()->getPathInfo() === '/' ? $file = '/index.html' : $event->getRequest()->getPathInfo();
+        $fullPath = '../pages/' . $event->getRequest()->getHttpHost() . $file;
+        if(file_exists($fullPath)){
+            echo file_get_contents($fullPath);
+            die();
         }
     }
 
