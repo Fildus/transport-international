577431f1c6b4d5596fba99e4c9f675d0243bf44a
david <davidgastaldello@msn.com>
2019-02-28 15:47:58 +0100

    fix(eventSubscriber)

--- src/EventSubscriber/RedirectSubscriber.php
+++ src/EventSubscriber/RedirectSubscriber.php
@@ -19,11 +19,11 @@ class RedirectSubscriber extends AbstractController implements EventSubscriberIn
      */
     public function onKernelController(FilterControllerEvent $event)
     {
-//        $kernelHost = $event->getRequest()->getHttpHost();
-//        if ((int)preg_match('#^www#', $kernelHost) === 0) {
-//            header('Location: ' . 'https://www.' . $kernelHost, true, 301);
-//            exit();
-//        }
+        $kernelHost = $event->getRequest()->getHttpHost();
+        if ((int)preg_match('#^www#', $kernelHost) === 0) {
+            header('Location: ' . 'https://www.' . $kernelHost, true, 301);
+            exit();
+        }
 
         $file = $event->getRequest()->getPathInfo() === '/' ? $file = '/index.html' : $event->getRequest()->getPathInfo();
         $fullPath = '../pages/' . str_replace('www.','',$event->getRequest()->getHttpHost()) . $file;
