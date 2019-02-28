bbb052083df02c094d46d98c3b3196130b95cb55
david <davidgastaldello@msn.com>
2019-02-28 14:49:18 +0100

    fix(contentType)

--- src/EventSubscriber/RedirectSubscriber.php
+++ src/EventSubscriber/RedirectSubscriber.php
@@ -6,6 +6,7 @@ use App\Controller\Front\ProfessionalProfileController;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 
@@ -27,6 +28,12 @@ class RedirectSubscriber extends AbstractController implements EventSubscriberIn
         $file = $event->getRequest()->getPathInfo() === '/' ? $file = '/index.html' : $event->getRequest()->getPathInfo();
         $fullPath = '../pages/' . str_replace('www.','',$event->getRequest()->getHttpHost()) . $file;
         if(file_exists($fullPath)){
+            if (preg_match('#.css$#',$file)){
+                header('Content-Type: text/css');
+            }
+            if (preg_match('#.js$#',$file)){
+                header('Content-Type: application/javascript');
+            }
             echo file_get_contents($fullPath);
             die();
         }
